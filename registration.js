61087335b2e77efa0d5b6ecf0874862f85ec2991
Cakebot <knowaguyemail@icloud.com>
2025-01-19 22:24:35 -0500

    add checkbox AGAIN

--- registration.js
+++ registration.js
@@ -5,3 +5,6 @@ function updateCheckboxes() {
       checkbox.checked = (checkbox.value == service);
     }
   }
+document.addEventListener("DOMContentLoaded", function () {
+    updateCheckboxes();
+});
